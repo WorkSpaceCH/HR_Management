b232532e809f316ca5260d371c1bf1878adce733
WorkSpaceCH <workspacefocus22@gmail.com>
2025-09-20 22:00:38 +0530

    Login Page

--- workhub-hrms/src/app/app.component.ts
+++ workhub-hrms/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'workhub-hrms';
+  isAuthRoute = false;
+  private routerSubscription: Subscription;
+
+  constructor(private router: Router) {
+    // Subscribe to router events to detect when we're on an auth route
+    this.routerSubscription = this.router.events
+      .pipe(filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isAuthRoute = event.url.startsWith('/auth');
+      });
+    
+    // Check current route on initialization
+    const currentUrl = this.router.url;
+    this.isAuthRoute = currentUrl.startsWith('/auth');
+  }
+
+  ngOnDestroy() {
+    // Clean up subscription to prevent memory leaks
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
 }
